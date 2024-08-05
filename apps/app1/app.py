27c35a89e250fb6b1d46a21fe8a4a3e8eb74f70f
Mat Silva <matsilva@hey.com>
2024-08-05 09:37:04 -0400

    feat: add transcript segment

--- apps/app1/app.py
+++ apps/app1/app.py
@@ -3,7 +3,8 @@ from libs.transcript_processor.whisper_processor import WhisperProcessor
 def main():
     import argparse
     parser = argparse.ArgumentParser(description="Process audio with Whisper.")
-    parser.add_argument('audio_path', type=str, help="Path to the audio file")
+    parser.add_argument('-i', 'input', type=str, help="Path to the input audio file")
+    parser.add_argument('-o', 'output', type=str, help="Desired path to transcription file")
     args = parser.parse_args()
 
     processor = WhisperProcessor(model_name="small")
